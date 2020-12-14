152020b6a81a6ef8086f5051b9acdf9577dfed8b
nikhilsmehta <67570213+nikhilsmehta@users.noreply.github.com>
2020-12-14 11:09:51 -0800

    Update MQ2Sensor.ino

--- MQ2Sensor.ino
+++ MQ2Sensor.ino
@@ -9,13 +9,13 @@ int redLed = 12;
 int greenLed = 11;
 //int buzzer = 10;
 int smokeA0 = A5;
-// Your threshold value
+
 int sensorThres = 400;
 
 void setup() {
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
-  //pinMode(buzzer, OUTPUT);
+  //pinMode(buzzer, OUTPUT); Buzzer not set up yet
   pinMode(smokeA0, INPUT);
   Serial.begin(9600);
 }
